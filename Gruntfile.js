54ad07641235d16c8958cc798c7311a805632f8d
jstenninger <johan.stenninger@ea.com>
2015-03-02 17:24:23 +0100

    Converting it to a proper npm project

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: false
+            },
+            build: {
+                files: {
+                    'donut.min.js': 'src/donut.js'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('install', ['uglify']);
+
+};
